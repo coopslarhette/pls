9a7417d0e3aaa052441d85008bb9f1ce86cc6422
coopslarhette <coopslarhette@gmail.com>
2021-01-25 20:22:33 -0800

    changes stretched a bit

--- homework1/src/exercises.js
+++ homework1/src/exercises.js
@@ -1,28 +1,28 @@
-function stretched(array) {
-  const repeat = (el, n, arr) => (n > 0 ? repeat(el, n - 1, [...arr, el]) : arr)
+export function stretched(array) {
+  // const repeat = (el, n, arr) => (n > 0 ? repeat(el, n - 1, [...arr, el]) : arr) hmmm which is better
   function recurse(base, n, prev) {
     if (base?.length > 0) {
-      return recurse(base.slice(1), n + 1, [...prev, ...repeat(base[0], n, [])])
+      return recurse(base.slice(1), n + 1, [...prev, ...Array(n).fill(base[0])])
     }
     return prev
   }
   return recurse(array, 1, [])
 }
 
-function* powers(base, limit) {
+export function* powers(base, limit) {
   for (let i = 0; base ** i <= limit; i += 1) {
     yield base ** i
   }
 }
 
-function say(previous) {
+export function say(previous) {
   if (previous) {
     return (current) => (current ? say(`${previous} ${current}`) : previous)
   }
   return ''
 }
 
-function topTenScorers(teamPlayerStats) {
+export function topTenScorers(teamPlayerStats) {
   return Object.entries(teamPlayerStats)
     .flatMap(([team, playerList]) => playerList.map((player) => [...player, team]))
     .filter((player) => player[1] >= 15)
@@ -31,8 +31,4 @@ function topTenScorers(teamPlayerStats) {
     .slice(0, 10)
 }
 
-function interpret() {}
-
-export {
-  say, powers, stretched, topTenScorers, interpret,
-}
+export function interpret() {}
