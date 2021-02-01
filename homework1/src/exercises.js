3b1635e0a7c959af2b5a640547232ba1135d914d
coopslarhette <coopslarhette@gmail.com>
2021-01-31 16:45:15 -0800

    adds interpret and lints

--- homework1/src/exercises.js
+++ homework1/src/exercises.js
@@ -1,5 +1,4 @@
 export function stretched(array) {
-  // const repeat = (el, n, arr) => (n > 0 ? repeat(el, n - 1, [...arr, el]) : arr) hmmm which is better
   function recurse(source, n, previous) {
     if (source?.length > 0) {
       return recurse(source.slice(1), n + 1, [...previous, ...Array(n).fill(source[0])])
@@ -23,13 +22,63 @@ export function say(previous) {
 }
 
 export function topTenScorers(teamPlayerStats) {
-  return Object.entries(teamPlayerStats)
-    .flatMap(([team, playerList]) => playerList.map((player) => [...player, team]))
-    // eslint-disable-next-line
-    .filter(([ , games, , ]) => games >= 15)
-    .map((player) => ({ name: player[0], ppg: player[2] / player[1], team: player[3] }))
-    .sort(({ ppg: a }, { ppg: b }) => b - a)
-    .slice(0, 10)
+  return (
+    Object.entries(teamPlayerStats)
+      .flatMap(([team, playerList]) => playerList.map((player) => [...player, team]))
+      // eslint-disable-next-line
+      .filter(([, games, ,]) => games >= 15)
+      .map((player) => ({ name: player[0], ppg: player[2] / player[1], team: player[3] }))
+      .sort(({ ppg: a }, { ppg: b }) => b - a)
+      .slice(0, 10)
+  )
 }
 
-export function interpret() {}
+export function interpret(program) {
+  const stack = []
+  const out = []
+  const twoOperandOperators = ['SWAP', '+', '-', '*', '/']
+  const oneOperandOperators = ['NEG', 'SQRT', 'PRINT', 'DUP']
+  const hasEnoughOperands = (op) => (twoOperandOperators.includes(op) && stack.length >= 2)
+    || (oneOperandOperators.includes(op) && stack.length >= 1)
+
+  const tokens = program.trim().split(' ')
+  const ops = {
+    NEG: () => stack.push(-1 * stack.pop()),
+    SQRT: () => stack.push(Math.sqrt(stack.pop())),
+    DUP: () => stack.push(stack[stack.length - 1]),
+    SWAP: () => stack.push(...stack.splice(stack.length - 2).reverse()),
+    PRINT: () => out.push(stack.pop()),
+    '+': () => {
+      const [y, x] = stack.splice(stack.length - 2)
+      stack.push(x + y)
+    },
+    '-': () => {
+      const [y, x] = stack.splice(stack.length - 2)
+      stack.push(x - y)
+    },
+    '*': () => {
+      const [y, x] = stack.splice(stack.length - 2)
+      stack.push(x * y)
+    },
+    '/': () => {
+      const [y, x] = stack.splice(stack.length - 2)
+      if (y === 0) throw Error('Can\'t divide by 0')
+      stack.push(x / y)
+    },
+  }
+
+  tokens.forEach((t) => {
+    const number = parseInt(t, 10)
+    if (!Number.isNaN(number)) {
+      stack.push(number)
+    } else if (t in ops) {
+      if (!hasEnoughOperands(t)) {
+        throw Error('Not enough operands')
+      }
+      ops[t]()
+    } else {
+      throw Error('Illegal Instruction')
+    }
+  })
+  return out
+}
