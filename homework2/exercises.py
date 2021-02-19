13cb9e52f983cfc5dddc5e66bd5c734f38cc02d3
coopslarhette <coopslarhette@gmail.com>
2021-02-18 22:55:18 -0800

    adds interpret

--- homework2/exercises.py
+++ homework2/exercises.py
@@ -1,3 +1,6 @@
+import math
+
+
 def stretched(array):
     result = []
     for index, element in enumerate(array):
@@ -32,5 +35,32 @@ def top_ten_scorers(data):
     return top_ten_sorted
 
 
-def interpret():
-    return None
+def interpret(program):
+    stack = []
+    tokens = program.split(' ')
+    two_operand_operators = ['SWAP', '+', '-', '*', '/']
+    one_operand_operators = ['NEG', 'SQRT', 'PRINT', 'DUP']
+    has_enough_operands = lambda op: (op in two_operand_operators and len(stack) >= 2) or (
+            op in one_operand_operators and len(stack) >= 1)
+    ops = {
+        'NEG': lambda: stack.append(-1 * stack.pop()),
+        'SQRT': lambda: stack.append(math.sqrt(stack.pop())),
+        'DUP': lambda: stack.append(stack[-1]),
+        'SWAP': lambda: stack.extend([stack.pop(), stack.pop()]),
+        '+': lambda: stack.append(stack.pop() + stack.pop()),
+        '-': lambda: stack.append(stack.pop() - stack.pop()),
+        '*': lambda: stack.append(stack.pop() * stack.pop()),
+        '/': lambda: stack.append(stack.pop() / stack.pop()),
+    }
+
+    for t in tokens:
+        if t.isdigit():
+            stack.append(int(t))
+        elif t in ops:
+            if not has_enough_operands(t):
+                raise ValueError('Not enough operands')
+            ops[t]()
+        elif t == 'PRINT':
+            yield stack.pop()
+        else:
+            raise ValueError('Illegal instruction')
